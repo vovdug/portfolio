c4f45db2729d5a377608dc46a8970f3047ae4d2f
Vladimir Duginov <vovv.dug@yandex.ru>
2024-06-25 20:28:59 +0300

    feat:add script for burger

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,11 +3,13 @@ const menu = document.querySelector("#menu");
 
 burger.addEventListener("click", () => {
     menu.classList.toggle("open");
+    burger.classList.toggle("active");
 })
 
 menu.addEventListener("click", (event) => {
     const isAnchorLink = event.target.closest("a");
     if (isAnchorLink) {
         menu.classList.remove("open");
+        burger.classList.remove("active");
     }
 })
